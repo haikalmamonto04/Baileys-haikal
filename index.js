ecad74e23f5a7886b379cd9bfe796186790775a3
Haikal Mamonto <haikalmamonto04@gmail.com>
2025-06-23 08:25:29 +0700

    Update fitur X

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const baileys = require('@whiskeysockets/baileys')
-const buttons = require('./lib/buttons')
+const buttons = require('./lib/buttons.js') // tambahkan ekstensi biar aman di semua runtime
 
+// Ekspor semua isi Baileys + tombol custom kamu
 module.exports = {
   ...baileys,
   ...buttons
